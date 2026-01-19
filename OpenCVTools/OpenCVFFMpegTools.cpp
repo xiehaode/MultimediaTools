1bb274d354aa0ac42fd84b9e1e4e031d6646a823
xiehaode <2802625868@qq.com>
2026-01-19 20:05:21 +0800

    first commit init env

---
+++ OpenCVTools/OpenCVFFMpegTools.cpp
@@ -0,0 +1,22 @@
+﻿// OpenCVTools.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "OpenCVTools.h"
+
+
+// 这是导出变量的一个示例
+OPENCVTOOLS_API int nOpenCVTools=0;
+
+// 这是导出函数的一个示例。
+OPENCVTOOLS_API int fnOpenCVTools(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+COpenCVTools::COpenCVTools()
+{
+    return;
+}
